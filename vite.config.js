f5094cbdf77b6986dbf18f817aa3eb3b550bd8a8
shubham <shubhampwr9945@gmail.com>
2024-10-15 00:27:00 +0530

    deploy vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/quick-bite-application/",
   plugins: [react()],
-  base: "/quick-bite/"
 })
